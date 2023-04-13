c31ff916bb4b50fc7a6d4214ce4eede833128516
BastianDev <blineaa@gmail.com>
2023-04-13 12:55:56 -0400

    Player Movement Restriction: Added Padding for Area Bounds, Including UI Area
    Extended the area movement restriction for the player to include padding around both the game area and the UI area. This prevents the player from reaching the edges of the screen, including any UI elements

--- Starfall/Assets/Scripts/Player.cs
+++ Starfall/Assets/Scripts/Player.cs
@@ -7,6 +7,11 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 12f;
+    
+    [SerializeField] private float paddingLeft;
+    [SerializeField] private float paddingRight;
+    [SerializeField] private float paddingTop;
+    [SerializeField] private float paddingBottom;
 
     private Vector2 _rawInput;
 
@@ -42,8 +47,8 @@ public class Player : MonoBehaviour
         
         Vector2 newPosition = new Vector2();
 
-        newPosition.x = Mathf.Clamp(transform.position.x + delta.x, _minBounds.x, _maxBounds.x);
-        newPosition.y = Mathf.Clamp(transform.position.y + delta.y, _minBounds.y, _maxBounds.y);
+        newPosition.x = Mathf.Clamp(transform.position.x + delta.x, _minBounds.x + paddingLeft, _maxBounds.x - paddingRight);
+        newPosition.y = Mathf.Clamp(transform.position.y + delta.y, _minBounds.y + paddingBottom, _maxBounds.y - paddingTop);
         
         transform.position = newPosition;
     }
