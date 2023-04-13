ced6b9ebc4bddd49cca3d9da2b47c3cf79f1039a
BastianDev <blineaa@gmail.com>
2023-04-13 12:36:20 -0400

    Camera Viewport Boundaries: Player Movement Update
    In this commit, I have implemented changes to bound the player's movement within the camera viewport. This ensures that the player remains within the visible area of the camera and prevents them from moving outside the bounds of the game screen

--- Starfall/Assets/Scripts/Player.cs
+++ Starfall/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,29 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 12f;
+
     private Vector2 _rawInput;
+
+    private Vector2 _minBounds;
+    private Vector2 _maxBounds;
+    
     void Update()
     {
         Move();
     }
 
+    private void Start()
+    {
+        InitBounds();
+    }
+    
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        _minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        _maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+    
     void OnMove(InputValue value)
     {
         _rawInput = value.Get<Vector2>();
@@ -20,7 +38,13 @@ public class Player : MonoBehaviour
 
     void Move()
     {
-        Vector3 delta = _rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = _rawInput * moveSpeed * Time.deltaTime;
+        
+        Vector2 newPosition = new Vector2();
+
+        newPosition.x = Mathf.Clamp(transform.position.x + delta.x, _minBounds.x, _maxBounds.x);
+        newPosition.y = Mathf.Clamp(transform.position.y + delta.y, _minBounds.y, _maxBounds.y);
+        
+        transform.position = newPosition;
     }
 }
